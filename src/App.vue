b8b8d0720180d5a547e7ce23bac4dd2ec3bb690c
Rakuraku Jyo <jyo.rakuraku@gmail.com>
2018-09-11 13:06:02 +0900

    Update for smaller phones

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ export default {
 
 <style>
 #app, button, input {
-  font-family: "Open Sans", "Source Han Sans", "Hiragino Sans", -apple-system, "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-family: "Open Sans", "Source Han Sans", -apple-system, "Helvetica Neue", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
