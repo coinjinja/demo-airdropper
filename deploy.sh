1a2e54e42d8d20ef29e2a0336d49d06eb62758c8
Jesse Zhu <jesse@itjesse.com>
2018-09-12 17:59:20 +0900

    update appId

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+scp -r dist/* su_yan@192.168.139.222:/code/dapp-demo/app/static/airdropper/
--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import coinview from '@coinjinja/coinview-sdk'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-coinview.init('R4Pa3Ev1').then(() => console.log('Coinview sdk inited.'))
-
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+coinview.init('njqNbqXQ').then(() => {
+  console.log('Coinview sdk inited.')
+  new Vue({
+    render: h => h(App)
+  }).$mount('#app')
+})
